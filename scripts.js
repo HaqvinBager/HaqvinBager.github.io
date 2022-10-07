6544f5869942b969a93a68e368e588b58e23d203
Haqvin Bager <50270210+HaqvinBager@users.noreply.github.com>
2022-10-07 15:59:24 +0200

    Added style.css and scripts.js to control the functions and style outside of each of the pages

---
+++ scripts.js
@@ -0,0 +1,25 @@
+function OpenPage(index)
+{
+    switch(index)
+    {
+
+        case 1:
+            window.location.href = "https://haqvinbager.github.io";
+            break;
+        case 2:
+            window.location.href = "https://haqvinbager.github.io/King";
+            break;
+        case 3:
+            window.location.href = "https://haqvinbager.github.io/TheGameAssembly";
+            break;
+        case 4:
+            window.location.href = "https://haqvinbager.github.io/Contact";
+            break;
+        case 5:
+            window.location.href = "https://haqvinbager.github.io/Personal";
+            break;
+        default:
+            window.location.href = "https://haqvinbager.github.io";
+            break;
+    }
+}
